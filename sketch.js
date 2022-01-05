5fc3e1dab64fc44c53127e02a0ea51339fc47efb
Finpofe <Brunorojo1@hotmail.com>
2022-01-05 13:48:58 -0600

    funcion keyPressed

--- sketch.js
+++ sketch.js
@@ -45,9 +45,9 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -75,4 +75,10 @@ Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY});
 
 function mouseReleased(){
 slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
